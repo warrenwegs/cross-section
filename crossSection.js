5ba5de910e925dcf0b60548c66735280ffc49c0d
Warren Wegner <warrenwegner@gmail.com>
2017-05-09 23:08:05 -0700

    adds ability to click to add points

--- crossSection.js
+++ crossSection.js
@@ -15,17 +15,27 @@ function getMousePos(canvas, evt) {
     };
 }
 
+function getSnapPoint(x, y) {
+    "use strict";
+    var proximity = 0.4, grid = 25;
+    var point = {};
+    if ((x % grid < (proximity * grid) || x % grid > (grid - proximity * grid)) && (y % grid > (proximity * grid) || y % grid < (grid - proximity * grid))) {
+        point.x = Math.floor(x / grid) * grid + Math.round(x % grid / grid) * grid;
+        point.y = Math.floor(y / grid) * grid + Math.round(y % grid / grid) * grid;
+    }
+    
+    return point;
+}
+
 function highlightPoint(canvas, x, y) {
     "use strict";
     var ctx = canvas.getContext('2d');
-    var proximity = 0.4, grid = 25;
-    
-    if ((x % grid < (proximity * grid) || x % grid > (grid - proximity * grid)) && ( y % grid > (proximity * grid) || y % grid < (grid - proximity * grid))) {
-        x = Math.floor(x / grid) * grid + Math.round(x % grid / grid) * grid;
-        y = Math.floor(y / grid) * grid + Math.round(y % grid / grid) * grid;
+    var point = getSnapPoint(x, y);
+    if (point) {
         ctx.beginPath();
-        ctx.arc(x, y, 5, 0, 2 * Math.PI);
+        ctx.arc(point.x, point.y, 5, 0, 2 * Math.PI);
         ctx.lineWidth = 5;
+        ctx.strokeStyle = '#ff0000';
         ctx.stroke();
         ctx.closePath();
     }
@@ -81,6 +91,22 @@ function drawGrid(canvas) {
     drawGridLines([major, minor], canvas);
 }
 
+function addPoint(point) {
+    window.points.push(point);
+}
+
+function drawPoints(canvas) {
+    var ctx = canvas.getContext('2d');
+    for (var i = 0; i < window.points.length; i++) {
+        ctx.beginPath();
+        ctx.arc(window.points[i].x, window.points[i].y, 2, 0, 2 * Math.PI);
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = '#000000';
+        ctx.stroke();
+        ctx.closePath();
+    }
+}
+
 var canvas = document.getElementById('my-canvas');
 var gridCanvas = document.getElementById('grid-canvas');
 var context = canvas.getContext('2d');
@@ -91,9 +117,17 @@ canvas.addEventListener('mousemove', function (evt) {
     var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
     writeMessage(canvas, message);
     highlightPoint(canvas, mousePos.x, mousePos.y);
+    drawPoints(canvas);
 }, false);
+
+canvas.addEventListener('click', function (evt) {
+    "use strict";
+    var mousePos = getMousePos(canvas, evt);
+    addPoint(getSnapPoint(mousePos.x, mousePos.y));
+})
 
 window.onload = function() {
     "use strict";
     drawGrid(gridCanvas);
+    window.points = [];
 };
